ec8c938b8805a5fb64729c566d054dd16394b168
Sam Thorogood <sam.thorogood@gmail.com>
2016-08-15 23:38:55 +1000

    demo commit

---
+++ device.js
@@ -0,0 +1,149 @@
+function shadowFor(el) {
+  if (el.attachShadowRoot) {
+    return el.attachShadowRoot();
+  } else if (el.createShadowRoot) {
+    return el.createShadowRoot();
+  }
+  throw new Error('shadow DOM unsupported');
+}
+
+function holderName() {
+  const el = document.createElement('div');
+  if (el.attachShadowRoot) {
+    return 'slot';
+  } else if (el.createShadowRoot) {
+    return 'content';
+  }
+  throw new Error('shadow DOM unsupported');
+}
+
+document.registerElement('ts-device', class extends HTMLElement {
+  createdCallback() {
+    const root = shadowFor(this);
+    const holder = holderName();
+
+    root.innerHTML = `
+<style>
+
+.holder {
+  color: #b5b5b5;
+  position: relative;
+  display: inline-block;
+  margin: 32px auto;
+}
+
+.device {
+  background: currentColor;
+  transform-style: preserve-3d;
+  z-index: 2;
+
+  // border hides glitch line between device and its edges
+  border: 1px currentColor solid;
+  border-radius: 1px;
+  border-left: 0;
+  border-right: 0;
+}
+
+/**
+ * The top and bottom of a device.
+ */
+.device:before, .device:after {
+  content: "";
+  border-radius: 100%;
+  width: 100%;
+  left: 0;
+  position: absolute;
+  z-index: -10;
+  background: transparent;
+  font-size: 25px;
+  height: 1em;
+  transition: all @duration-change;
+}
+.device:before {
+  transform: translate(0, -50%);
+  border-top: 1em solid currentColor;
+}
+.device:after {
+  bottom: 0;
+  transform: translate(0, +50%);
+  border-bottom: 1em solid currentColor;
+}
+
+.camera {
+  position: absolute;
+  top: 0;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 24px;
+  height: 24px;
+  background: #c2c2c2;
+  border-radius: 100px;
+  transition: all @duration-change;
+}
+
+.size {
+  display: table-cell;  /* sort of like overflow: hidden, but allows 3d transforms within! */
+  position: relative;
+  background: white;
+  transition: width @duration-change, height @duration-change;
+
+  width: 320px;
+  height: 480px;
+}
+.border {
+  transition: padding @duration-change;
+  padding: 25px 16px;
+}
+
+.shadow {
+  position: absolute;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  z-index: -1;
+  transform: translate3d(0, 0, -20px);
+  color: #717171;
+  background: currentColor;
+}
+
+.solid {
+  z-index: 1; /* "above" shadow */
+}
+
+.solid:after, .solid:before {
+  content: '';
+  position: absolute;
+  background: currentColor;
+  height: 20px;
+  left: 0;
+  right: 0;
+  transform: rotateX(90deg);
+  transform-origin: 50% 0;
+  transition: all @duration-change;
+}
+.solid:after {
+  top: -2px;
+}
+.solid:before {
+  bottom: -2px;
+  transform: translateY(100%) rotateX(90deg);
+}
+
+</style>
+<div class="holder">
+  <div class="device">
+    <div class="device shadow">
+      <div class="solid"></div>
+    </div>
+    <div class="camera"></div>
+    <div class="border">
+      <div class="size" id="contents">
+        <${holder}></${holder}>
+      </div>
+    </div>
+  </div>
+</div>
+    `;
+
+  }
+});
