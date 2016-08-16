7d5430294150ab824c2cb56ab9f12622bddc97f1
Sam Thorogood <sam.thorogood@gmail.com>
2016-08-16 12:01:06 +1000

    support tablet/round

--- device.js
+++ device.js
@@ -18,6 +18,19 @@ function holderName() {
 }
 
 document.registerElement('ts-device', class extends HTMLElement {
+  constructor() {
+    super();
+    this.device_ = null;
+  }
+
+  attributeChangedCallback(attrName, oldValue, newValue) {
+    switch (attrName) {
+    case 'device':
+      this.holder_.setAttribute('device', newValue);
+      break;
+    }
+  }
+
   createdCallback() {
     const root = shadowFor(this);
     const holder = holderName();
@@ -37,8 +50,9 @@ document.registerElement('ts-device', class extends HTMLElement {
   transform-style: preserve-3d;
   z-index: 2;
 
-  // border hides glitch line between device and its edges
+  /* border hides glitch line between device and its edges */
   border: 1px currentColor solid;
+  margin: -1px 0;
   border-radius: 1px;
   border-left: 0;
   border-right: 0;
@@ -49,15 +63,14 @@ document.registerElement('ts-device', class extends HTMLElement {
  */
 .device:before, .device:after {
   content: "";
-  border-radius: 100%;
   width: 100%;
   left: 0;
   position: absolute;
   z-index: -10;
   background: transparent;
-  font-size: 25px;
   height: 1em;
-  transition: all @duration-change;
+  transition: all 0.5s;
+  border-radius: 100%;
 }
 .device:before {
   transform: translate(0, -50%);
@@ -71,28 +84,27 @@ document.registerElement('ts-device', class extends HTMLElement {
 
 .camera {
   position: absolute;
-  top: 0;
+  top: 10px;
   left: 50%;
   transform: translate(-50%, -50%);
   width: 24px;
   height: 24px;
   background: #c2c2c2;
   border-radius: 100px;
-  transition: all @duration-change;
+  transition: all 0.5s;
 }
 
 .size {
   display: table-cell;  /* sort of like overflow: hidden, but allows 3d transforms within! */
   position: relative;
   background: white;
-  transition: width @duration-change, height @duration-change;
+  transition: width 0.5s, height 0.5s;
 
   width: 320px;
   height: 480px;
 }
 .border {
-  transition: padding @duration-change;
-  padding: 25px 16px;
+  padding: 40px 16px;
 }
 
 .shadow {
@@ -119,7 +131,7 @@ document.registerElement('ts-device', class extends HTMLElement {
   right: 0;
   transform: rotateX(90deg);
   transform-origin: 50% 0;
-  transition: all @duration-change;
+  transition: all 0.5s;
 }
 .solid:after {
   top: -2px;
@@ -127,6 +139,26 @@ document.registerElement('ts-device', class extends HTMLElement {
 .solid:before {
   bottom: -2px;
   transform: translateY(100%) rotateX(90deg);
+}
+
+/** tablet device */
+
+.holder[device="tablet"] .device:before {
+  border-radius: 1000px;
+}
+.holder[device="tablet"] .device:after {
+  border-radius: 1000px;
+}
+
+.holder[device="tablet"] .solid:after, .holder[device="tablet"] .solid:before {
+  left: 6px;
+  right: 6px;
+}
+.holder[device="tablet"] .solid:after {
+  top: -13px;
+}
+.holder[device="tablet"] .solid:before {
+  bottom: -13px;
 }
 
 </style>
@@ -145,5 +177,9 @@ document.registerElement('ts-device', class extends HTMLElement {
 </div>
     `;
 
+    this.holder_ = root.querySelector('div.holder');
+
+    const attrs = 'device';
+    attrs.split().forEach(attr => this.attributeChangedCallback(attr, undefined, this.getAttribute(attr)));
   }
 });
