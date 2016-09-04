e0ebff35826393557253d576ef29a1d16fa99d70
Sam Thorogood <sam.thorogood@gmail.com>
2016-09-04 17:43:49 +1000

    initial scripts

---
+++ scripts.js
@@ -0,0 +1,15 @@
+
+const scripts = {
+  'TS003': [
+    {'scene': 'flat'},
+    {'class': 'highlight'},
+    {'class': 'highlight height'},
+    {'class': 'highlight height nomargin'},
+    {'class': 'height nomargin'},
+    {'scene': null, 'overflow': true},
+    {'class': 'nomargin'},
+    {'class': 'nomargin scroll'},
+    {'class': 'nomargin bgcolor scroll'},
+    {'class': 'nomargin bgcolor'},
+  ]
+};
