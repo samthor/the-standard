8c2709dec9bbf672fb5161151c8a942bd5a65628
Sam Thorogood <sam.thorogood@gmail.com>
2017-06-16 12:53:36 +1000

    more to modules

---
+++ elements.js
@@ -0,0 +1,13 @@
+
+// ES6 module imports for elements, side-effects only
+
+import './elements/device.js';
+import './elements/feature.js';
+import './elements/lines.js';
+import './elements/page.js';
+import './elements/scene.js';
+import './elements/scroll.js';
+
+import './elements/ui/header.js';
+import './elements/ui/loader.js';
+import './elements/ui/playpause.js';
